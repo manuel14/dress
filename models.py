f07ac2c01a919a170b343212d503da8a7141a903
Franco Escobar <franco_m77@hotmail.com>
2013-03-21 18:49:17 -0300

    agregados metodos de las clases ListaClientes y ListaPrendas

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import datetime
+import string
 import wx
 from wx.lib.pubsub import Publisher as pub
 
@@ -6,9 +7,10 @@ class Compra:
     """
     """
 
-    def __init__(self, prenda, cliente, precio):
+    def __init__(self, prenda, cliente, precio, nombrePrenda):
         self.fecha = datetime.date.today()
         self.prenda = prenda
+        self.nombrePrenda = nombrePrenda
         self.cliente = cliente
         self.precio = precio
 
@@ -21,6 +23,7 @@ class Condicional:
         self.fecha = datetime.date.today()
         self.prenda = prenda
         self.cliente = cliente
+
 
 class Movimiento:
     """
@@ -43,8 +46,6 @@ class Movimiento:
         return resul
 
 
-
-
 class Entrega:
     """
     """
@@ -59,7 +60,7 @@ class Cliente:
     """
 
     def __init__(self, dni, nombre, telefono, email):
-        self._dni = dni
+        self.__dni = dni
         self.nombre = nombre
         self.telefono = telefono
         self.email = email
@@ -115,22 +116,45 @@ class Cliente:
     def getMovimietos(self):
         movimientos = []
 
-        for compra in compras:
-            movim = Movimiento(compra.fecha, "Compra", compra.prenda, compra.precio)
+        for compra in self.compras:
+            movim = Movimiento(compra.fecha, "Compra", compra.prenda + compra.nombrePrenda, compra.precio)
             movimientos.append(movim)
 
-        for entrega in entregas:
+        for entrega in self.entregas:
             movim = Movimiento(entrega.fecha, "Entrega", "-", entrega.monto)
             movimientos.append(movim)
 
 
-        for condicional in condicionales:
-            movim = Movimiento(condicional.fecha, "Condicional", condicional.prenda, 0)
+        for condicional in self.condicionales:
+            movim = Movimiento(condicional.fecha, "Condicional", condicional.prenda + compra.nombrePrenda, 0)
             movimientos.append(movim)
 
         movimientos.sort()
 
         return movimientos
+
+    def getSaldo(self):
+        deuda = 0
+        credito = 0
+
+        for compra in self.compras:
+            deuda = deuda + compra.precio
+
+        for entrega in self.entregas:
+            credito = credito + entrega.monto
+
+        saldo = credito - deuda
+
+        return saldo
+    
+    def getEstadoCliente(self):
+        # no se como hacer la comparacion entre fechas, la idea es que si getSaldo = 0
+        # o fecha_ultima_entrega es menor a un mes,  que sea "al dia", si getSaldo > 0
+        # y fecha_ultima_entrega es mayor a un mes pero menor a dos meses sea "tardio"
+        # y si getSaldo > 0 fecha_ultima_entrega es mayor a dos meses sea "moroso"
+
+    def getDni(self):
+        return self.__dni
 
 
 class Prenda:
@@ -138,7 +162,7 @@ class Prenda:
     """
 
     def __init___(self, codigo, nombre, talle, costo, precio, descripcion):
-        self._codigo = codigo
+        self.__codigo = codigo
         self.nombre = nombre
         self.talle = talle
         self.costo = costo
@@ -146,7 +170,6 @@ class Prenda:
         self. descripcion = descripcion
         self.vendida = False
         self.condicional = False
-        self.pagada = False
 
     def setNombre(self, nombre):
         self.nombre = nombre
@@ -176,23 +199,144 @@ class Prenda:
         self.condicional = condicional
         pub.sendMessaje("CAMBIO PRENDA", self)
 
-    def setPagada(self, pagada):
-        self.pagada = pagada
-        pub.sendMessaje("CAMBIO PRENDA", self)
+    def getEstadoPrenda(self):
+        estado = "disponible"
+        
+        if self.vendida:
+            estado = "vendida"
+        elif self.condicional:
+            estado = "condicional"
 
+        return estado
 
+    def getCodigo(self):
+        return self.__codigo
 
 class ListaClientes:
 
 
     def __init__(self):
-        self._clientes = []
+        self.__clientes = []
 
+    def addCliente(self, cliente):
+        self.__clientes.append(cliente)
+        pub.sendMessaje("CLIENTE AGREGADO", self)
+
+    def deleteCliente(self, cliente):
+        self.__clientes.remove(cliente)
+        pub.sendMessaje("CLIENTE ELIMINADO", self)
+
+    def getClientes(self): 
+        return self.__clientes
+
+    def getClientesMorosos(self):
+        clientes_morosos = []
+
+        for cliente in __clientes:
+            if cliente.getEstadoCliente = "moroso":
+                clientes_morosos.append(cliente)
+
+        return clientes_morosos
+
+    def getClientesAlDia(self):
+        clientes_al_dia = []
+
+        for cliente in __clientes:
+            if cliente.getEstadoCliente = "al dia":
+                clientes_al_dia.append(cliente)
+
+        return clientes_al_dia
+
+    def getClientesTardios(self):
+        clientes_tardios = []
+
+        for cliente in __clientes:
+            if cliente.getEstadoCliente = "tardio"
+                clientes_tardios.append(moroso)
+
+    def getClientePorDni(self, dni):
+        clientes_encontrados = []
+        
+        for cliente in self.__clientes:
+            if cliente.getDni() == dni:
+                clientes_encontrados.append(cliente)
+
+        return clientes_encontrados
+
+    def getClientePorNombre(self, nombre):
+
+        clientes_encontrados = []
+    
+        for cliente in self.__clientes:
+            if string.find(cliente.nombre, nombre) > 0:
+                clientes_encontrados.append(cliente)
+
+        return clientes_encontrados
+                
 
 class ListaPrendas:
 
 
     def __init__(self):
-        self._prendas = []
+        self.__prendas = []
+
+    def addPrenda(self, prenda):
+        self.__prendas.append(prenda)
+        pub.sendMessaje("PRENDA AGREGADA", self)
+
+    def deletePrenda(self, prenda):
+        self.__prendas.remove(prenda)
+        pub.sendMessaje("PRENDA ELIMINADA", self)
+
+    def getPrendas(self): 
+        return self.__prendas
+
+    def getPrendasVendidas(self):
+        prendas_vendidas = []
+
+        for prenda in __prendas:
+            if prenda.getEstadoPrenda = "vendida":
+                prendas_vendidas.append(prenda)
+
+        return prendas_vendidas
+
+    def getPrendasDisponibles(self):
+        prendas_disponibles = []
+
+        for prenda in __prendas:
+            if prenda.getEstadoPrenda = "disponible":
+                prendas_disponibles.append(prenda)
+
+        return prendas_disponibles
+
+    def getPrendasCondicionales(self):
+        prendas_condicionales = []
+
+        for prenda in __prendas:
+            if prenda.getEstadoPrenda = "condicional":
+                prendas_condicionales.append(prenda)
+
+        return prendas_condicionales
+
+    def getPrendaPorCodigo(self, codigo):
+        prendas_encontradas = []
+        
+        for prenda in self.__prendas:
+            if prenda.getCodigo() == codigo:
+                prendas_encontradas.append(prenda)
+
+        return prendas_encontradas
+
+    def getPrendaPorNombre(self, nombre):
+        prendas_encontradas = []
+    
+        for prenda in self.__prendas:
+            if string.find(prenda.nombre, nombre) > 0:
+                prendas_encontradas.append(prenda)
+
+        return prendas_encontradas
+
+
+
 
 
