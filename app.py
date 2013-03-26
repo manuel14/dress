8510f0bc8fca596f431a0b2b1a578fd2c4193f0f
Leonardo Ferreyra <leferreyra@gmail.com>
2013-03-26 19:13:25 -0300

    Agregar columnas lista_clientes en MainFrame

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import wx
 import models
 import views
+import data
 from wx.lib.pubsub import Publisher as pub
 import cPickle as pickle
+
+from views.MainFrame import MainFrame
 
 class AppController:
     """
@@ -12,19 +15,35 @@ class AppController:
     def __init__(self, app):
 
         self.app = app
+        self.data = data.load()
+
+	self.main_window = MainFrame(None, -1, "A&M Moda")
 
 	self.initUi()
-	self.connectEvents()
+
+	self.main_window.Show()
 
 
     def initUi(self):
 
-        pass
+        # Cargar las listas principales, lista_clientes y lista_prendas
+        # =============================================================
 
+        # lista_clientes	
+	lista_clientes = self.main_window.lista_clientes
 
-    def connectEvents(self):
+	# Agregar columnas a lista_clientes
+	lista_clientes.InsertColumn(0, "DNI", width=100)
+	lista_clientes.InsertColumn(1, "Nombre", width=300)
+	lista_clientes.InsertColumn(2, "Telefono", width=200)
+	lista_clientes.InsertColumn(3, "Saldo")
+
+	# Agregar items a lista_clientes
+	for item lista_clientes.getClientes():
+	    
+            lista_clientes
+	
         
-	pass
 
 
 if __name__=='__main__':
