ff14b571136419c5492a56b36e50de1eb7e3d3ab
Nicomagg <nicolas_maggione_27@hotmail.com>
2013-04-05 18:42:59 -0300

    Agregue giladas nomas en el app

--- app.py
+++ app.py
@@ -58,7 +58,6 @@ class AppController:
         self.main_window.boton_nuevo_clientes.Bind(wx.EVT_BUTTON, self.nuevoCliente)
 
 
-
     def MostrarDetallePrenda(self):
 
         seleccionado = self.main_window.lista_prendas.getFocusedItem()
@@ -85,4 +84,46 @@ if __name__=='__main__':
     app = wx.App(False)
     controller = AppController(app)
     app.MainLoop()
+
+#enable o algo asi
+#Controlador Detalle cliente
+class DetalleCLienteController:
+    """
+    COntrolador Detalle Cliente
+    """
+
+    def __init__(self, cliente):
+
+        self.cliente = cliente
+        self.data = data.load()
+
+        self.main_window = MainFrame(None, -1, "A&M Moda")
+
+        self.initUi()
+
+        self.main_window.Show()
+
+    def initUi(self):
+
+        # Cargar las listas Detalle_Cliente
+        # =============================================================
+
+        # lista_clientes    
+        lista_clientes = self.main_window.lista_clientes
+
+        # Agregar columnas a lista_clientes
+        lista_clientes.InsertColumn(0, "DNI", width=100)
+        lista_clientes.InsertColumn(1, "Nombre", width=300)
+        lista_clientes.InsertColumn(2, "Telefono", width=200)
+        lista_clientes.InsertColumn(3, "Saldo")
+
+        # Agregar items a lista_clientes
+        for item lista_clientes.getClientes():
+        
+            lista_clientes
+
+#fecha, tipo:va si es condi, compra o pago...., 
+#codigo prenda: el codigo pero si es pago no iene codi, 
+#monto: si es condicional no tine monto
+
 
